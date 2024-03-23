fbdd5d3ef872a8a9c9acebf213ad893843015e9a
facuagueria <facundoagueria1@gmail.com>
2024-03-23 20:13:50 -0300

    refactor 📦: organize test suite into describe blocks

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -17,54 +17,65 @@ describe('WordleBoard', () => {
     await guessInput.trigger('keydown.enter')
   }
 
-  test('a victory message appears when the user makes a guess that matches the word of the day', async () => {
-    //Act
-    await playerSubmitsGuess(wordOfTheDay)
+  describe('End of the game messages', () => {
+    test('a victory message appears when the user makes a guess that matches the word of the day', async () => {
+      //Act
+      await playerSubmitsGuess(wordOfTheDay)
 
-    //Assert
-    expect(wrapper.text()).toContain(VICTORY_MESSAGE)
+      //Assert
+      expect(wrapper.text()).toContain(VICTORY_MESSAGE)
+    })
+
+    test('a defeat message appears if the user makes a guess that is incorrect', async () => {
+      await playerSubmitsGuess('ANOTHER THING')
+
+      expect(wrapper.text()).toContain(DEFEAT_MESSAGE)
+    })
+
+    test('no end-of-game message appears if the user has not yet made a guess', async () => {
+      expect(wrapper.text()).not.toContain(VICTORY_MESSAGE)
+      expect(wrapper.text()).not.toContain(DEFEAT_MESSAGE)
+    })
   })
 
-  test('a defeat message appears if the user makes a guess that is incorrect', async () => {
-    await playerSubmitsGuess('ANOTHER THING')
+  describe('Rules for defining the word of the day', () => {
+    test('if a word of the day provided does not have exactly 5 characters, a warning is emitted', async () => {
+      console.warn = vi.fn()
 
-    expect(wrapper.text()).toContain(DEFEAT_MESSAGE)
+      mount(WordleBoard, { props: { wordOfTheDay: 'FLY' } })
+
+      expect(console.warn).toHaveBeenCalled()
+    })
+
+    test('if the word of the day is not all in uppercase, a warning is emitted', async () => {
+      console.warn = vi.fn()
+
+      mount(WordleBoard, { props: { wordOfTheDay: 'tests' } })
+
+      expect(console.warn).toHaveBeenCalled()
+    })
+
+    test('if the word of the day is not a real English world, a warning is emitted', async () => {
+      console.warn = vi.fn()
+
+      mount(WordleBoard, { props: { wordOfTheDay: 'QWERT' } })
+
+      expect(console.warn).toHaveBeenCalled()
+    })
+
+    test('no warning is emitted if the word of the day is valid', async () => {
+      console.warn = vi.fn()
+
+      mount(WordleBoard, { props: { wordOfTheDay: 'TESTS' } })
+
+      expect(console.warn).not.toHaveBeenCalled()
+    })
   })
 
-  test('no end-of-game message appears if the user has not yet made a guess', async () => {
-    expect(wrapper.text()).not.toContain(VICTORY_MESSAGE)
-    expect(wrapper.text()).not.toContain(DEFEAT_MESSAGE)
-  })
-
-  test('if a word of the day provided does not have exactly 5 characters, a warning is emitted', async () => {
-    console.warn = vi.fn()
-
-    mount(WordleBoard, { props: { wordOfTheDay: 'FLY' } })
-
-    expect(console.warn).toHaveBeenCalled()
-  })
-
-  test('if the word of the day is not all in uppercase, a warning is emitted', async () => {
-    console.warn = vi.fn()
-
-    mount(WordleBoard, { props: { wordOfTheDay: 'tests' } })
-
-    expect(console.warn).toHaveBeenCalled()
-  })
-
-  test('if the word of the day is not a real English world, a warning is emitted', async () => {
-    console.warn = vi.fn()
-
-    mount(WordleBoard, { props: { wordOfTheDay: 'QWERT' } })
-
-    expect(console.warn).toHaveBeenCalled()
-  })
-
-  test('no warning is emitted if the word of the day is valid', async () => {
-    console.warn = vi.fn()
-
-    mount(WordleBoard, { props: { wordOfTheDay: 'TESTS' } })
-
-    expect(console.warn).not.toHaveBeenCalled()
+  describe('Player input', () => {
+    test.todo('player guesses are limited to 5 letters')
+    test.todo('player guesses can only be submitted if they are real words')
+    test.todo('player guesses are not case-sensitive')
+    test.todo('player guesses can only contain letters')
   })
 })
