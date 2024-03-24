3b07531137fe29a6ea9b50fcb9f4729000cdbf17
facuagueria <facundoagueria1@gmail.com>
2024-03-24 18:39:22 -0300

    style 💎: WordleBoard

--- src/components/WordleBoard.vue
+++ src/components/WordleBoard.vue
@@ -15,9 +15,39 @@ const wordSubmitted = ref('')
 </script>
 
 <template>
-  <guess-input @guess-submitted="(guess: string) => (wordSubmitted = guess)" />
-  <p
-    v-if="wordSubmitted.length > 0"
-    v-text="wordSubmitted === wordOfTheDay ? VICTORY_MESSAGE : DEFEAT_MESSAGE"
-  />
+  <main>
+    <guess-input @guess-submitted="(guess: string) => (wordSubmitted = guess)" />
+    <p
+      v-if="wordSubmitted.length > 0"
+      class="end-of-game-message"
+      v-text="wordSubmitted === wordOfTheDay ? VICTORY_MESSAGE : DEFEAT_MESSAGE"
+    />
+  </main>
 </template>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 3rem;
+}
+
+.end-of-game-message {
+  font-size: 3rem;
+  animation: end-of-game-message-animation 700ms forwards;
+  white-space: nowrap;
+  text-align: center;
+}
+
+@keyframes end-of-game-message-animation {
+  0% {
+    opacity: 0;
+    transform: rotateZ(0);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(2rem);
+  }
+}
+</style>
