4967973840b23f6de2f1100c38a12da925909b7b
facuagueria <facundoagueria1@gmail.com>
2024-03-24 21:01:35 -0300

    chore: stop submitting guess in tests related to guesses

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -127,14 +127,14 @@ describe('WordleBoard', () => {
     })
 
     test('player guesses can only contain letters', async () => {
-      await playerTypesAndSubmitsGuess('H3!RT')
+      await playerTypesGuess('H3!RT')
 
       expect(wrapper.find<HTMLInputElement>('input[type="text"]').element.value).toEqual('HRT')
     })
 
     test.skip('non-letters characters do not render on the screen while being typed', async () => {
-      await playerTypesAndSubmitsGuess('12')
-      await playerTypesAndSubmitsGuess('123')
+      await playerTypesGuess('12')
+      await playerTypesGuess('123')
 
       expect(wrapper.find<HTMLInputElement>('input[type="text"]').element.value).toEqual('')
     })
